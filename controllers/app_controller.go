0525427dfe027c2bf349917768380788251e0ba9
Miguel Elias dos Santos <migueleliasweb@gmail.com>
2022-11-26 00:28:31 +1100

    Begin reconcilers implementation

--- controllers/app_controller.go
+++ controllers/app_controller.go
@@ -11,18 +11,28 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	policyv1 "k8s.io/api/policy/v1beta1"
 
 	platformv1beta1 "migueleliasweb.github.io/api/v1beta1"
 )
 
-type DryRunnableReconciler interface {
+type SecondaryReconcilers interface {
+	ReconcileWithApp(
+		ctx context.Context,
+		req ctrl.Request,
+		app platformv1beta1.App,
+	) (ctrl.Result, error)
 }
 
 // AppReconciler reconciles a App object
 type AppReconciler struct {
-	client.Client
+	Client               client.Client
 	Scheme               *runtime.Scheme
-	SecondaryReconcilers DryRunnableReconciler
+	SecondaryReconcilers []reconcile.Reconciler
 }
 
 //+kubebuilder:rbac:groups=platform.migueleliasweb.github.io,resources=apps,verbs=get;list;watch;create;update;patch;delete
@@ -37,11 +47,10 @@ type AppReconciler struct {
 func (r *AppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
-	// TODO(user): your logic here
-
-	r.Client.Create(context.Background(), nil, &client.CreateOptions{
-		DryRun: client.DryRunAll,
-	})
+	// fetch current definition
+	// set status to "reconciling"?
+	// call secondary reconcilers
+	// done
 
 	return ctrl.Result{}, nil
 }
@@ -50,5 +59,11 @@ func (r *AppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 func (r *AppReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&platformv1beta1.App{}).
+		Owns(&appsv1.Deployment{}).
+		Owns(&appsv1.ReplicaSet{}).
+		Owns(&corev1.Pod{}).
+		Owns(&policyv1.PodDisruptionBudget{}).
+		Owns(&corev1.ServiceAccount{}).
+		// Owns(&corev1.Service{}).
 		Complete(r)
 }
